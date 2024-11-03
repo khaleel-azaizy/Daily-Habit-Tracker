7c49589d648cbcb50dfa6f33abd0f4a4d36455f2
khaleel-azaizy <Khaleelazaizy@gmail.com>
2024-11-03 21:26:12 +0200

    updating the Ui

--- server/Router.js
+++ server/Router.js
@@ -54,8 +54,85 @@ app.post('/login', (req, res) => {
         res.json(200)
       }
      })
+     .catch(err => {
+      res.status(500).json({error: 'Could not fetch the document'})
+    })
 
     });
      
-  
+    app.post('/add-event',(req,res) =>{
+      const Event = req.body;
+      console.log('Received event request with:',  Event); 
+      db.collection('UserEvents').insertOne(Event)
+      .then(doc =>{
+        if(doc){   
+          res.status(200).json(doc)
+        } else{
+          res.json(500)
+        }
+       })
+       .catch(err => {
+        res.status(500).json({error: 'Could not fetch the document'})
+      })
 
+
+    })
+  
+    app.get('/get-events', async (req, res) => {
+      try {
+       
+        const events = await db.collection('UserEvents').find().toArray(); 
+        
+        res.status(200).json(events); 
+      } catch (error) {
+        console.error('Error fetching events:', error);
+        res.status(500).json({ error: 'Failed to fetch events' });
+      }
+    });
+
+    app.delete('/delete-event/:id', (req, res) => {
+      const event = req.body;
+      const eventId = req.params.id;
+      console.log('Received delete event request with:',  event); 
+      db.collection('UserEvents').deleteOne({ id: eventId })
+        .then(() => {
+          res.status(200).send({ message: 'Event deleted successfully' });
+        })
+        .catch((err) => {
+          res.status(500).send({ error: 'Failed to delete event', details: err });
+        });
+    });
+
+    app.put('/update-event/:id', async (req, res) => {
+      try {
+        const eventId = req.params.id;
+        const updatedData = req.body;
+        
+        console.log('Received updated data:',updatedData)
+        if (!updatedData.title || !updatedData.start) {
+          return res.status(400).send({ error: 'Missing required fields' });
+        }
+        
+        const result = await db.collection('UserEvents').updateOne(
+          { id: eventId },
+          {
+            $set: {
+              title: updatedData.title,
+              start: updatedData.start,
+              end: updatedData.end,
+              allDay: updatedData.allDay,
+            },
+          }
+        );
+        
+
+        if (result.modifiedCount === 0) {
+          return res.status(404).send({ error: 'Event not found' });
+        }
+    
+        res.status(200).send({ message: 'Event updated successfully' });
+      } catch (error) {
+        console.error('Error updating event:', error);
+        res.status(500).send({ error: 'Failed to update event', details: error.message });
+      }
+    });
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,35 @@ import FullCalendar from "@fullcalendar/react";
 import dayGrid from "@fullcalendar/daygrid";
 import timeGrid from "@fullcalendar/timegrid";
 import interaction from "@fullcalendar/interaction";
-import { useState, useRef } from "react";
+import { useState, useRef,useEffect } from "react";
 
 export default function Home() {
   const calendarRef = useRef(null);
   const [modal, setModal] = useState(false);
   const [title, setTitle] = useState('');
   const [selectedDateInfo, setSelectedDateInfo] = useState(null); 
+  const [events, setEvents] = useState([]);
+  const [upcomingEvents,setUpcomingevents]=useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost:4000/get-events') 
+      .then((response) => response.json())
+      .then((data) => {
+        const sortedEvents = data.sort((a, b) => new Date(a.start) - new Date(b.start));
+        setEvents(sortedEvents);
+        
+      })
+      .catch((error) => {
+        console.error('Error fetching events:', error);
+      });
+  }, []);
+ 
+  useEffect(() => {
+    const currentDate = new Date();
+    const upcomingEvents = events.filter((event) => new Date(event.start) >= currentDate);
+    setUpcomingevents(upcomingEvents)
+  }, [events]);
+  
   const handleClick = () => {
     setModal(!modal);
   };
@@ -26,13 +47,26 @@ export default function Home() {
     calendarApi.unselect();
 
     if (title) {
-      calendarApi.addEvent({
-        id: `${selectedDateInfo.dateStr}-${title}`,
-        title,
-        start: selectedDateInfo.startStr,
-        end: selectedDateInfo.endStr,
-        allDay: selectedDateInfo.allDay,
-      });
+    const newEvent = {
+      id: new Date().getTime().toString(),
+      title,
+      start: selectedDateInfo.startStr,
+      end: selectedDateInfo.endStr,
+      allDay: selectedDateInfo.allDay,
+    };
+  
+      calendarApi.addEvent(newEvent);
+
+      fetch('http://localhost:4000/add-event',{
+        method:'post',
+        headers:{"Content-Type": "application/json"},
+        body:JSON.stringify(newEvent)
+     }).then(()=>{
+        console.log('new event added');
+        setEvents((prevEvents) => [...prevEvents, newEvent]);
+        
+     })
+
       setModal(false); 
       setTitle(''); 
     }
@@ -40,11 +74,53 @@ export default function Home() {
 
   const handleEventRemove = (eventClick) => {
     if (window.confirm("Are you sure you want to delete this event?")) {
+      const eventId = eventClick.event.id;
+
+      fetch(`http://localhost:4000/delete-event/${eventId}`, {
+        method: 'DELETE',
+        headers: { "Content-Type": "application/json" },
+        body:JSON.stringify(eventClick.event)
+      }).then(() => {
+        console.log('Event deleted');
+      }).catch(err => {
+        console.error('Error deleting event:', err);
+      });
       eventClick.event.remove();
-    }
+  };
+  
+  }
+
+  const handleEventDrop = (info) => {
+   
+    const updatedEvent = {
+      id: info.event.id,
+      title: info.event.title,
+      start: info.event.startStr,
+      end: info.event.endStr,
+      allDay: info.event.allDay,
+    };
+
+    fetch(`http://localhost:4000/update-event/${info.event.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedEvent),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('Event updated successfully');
+          
+        } else {
+          console.error('Failed to update event');
+        }
+      })
+      .catch((error) => {
+        console.error('Error updating event:', error);
+      });
   };
 
-  
+
   if (modal) {
     document.body.classList.add('active-modal');
   } else {
@@ -53,34 +129,59 @@ export default function Home() {
 
   return (
     <div className="home">
-      <h2>Welcome to Daily Habit</h2>
+ 
+  <div className="home-container">
+    <div className="all-events">
+      <h2>upcoming Events</h2>
+      {upcomingEvents.map((event) => (
+        <div className="event-preview" key={event.id}>
+          <div className="event-details">
+            <h3>{event.title}</h3>
+            <h4>{event.start}</h4>
+          </div>
+        </div>
+      ))}
+    </div>
+
+    <div className="calendar-container">
       <FullCalendar
         ref={calendarRef}
-        plugins={[dayGrid, timeGrid,interaction]}
+        plugins={[dayGrid, timeGrid, interaction]}
         initialView="dayGridMonth"
         selectable={true}
-        select={handleDateSelect} 
+        editable={true}
+        eventDrop={handleEventDrop}
+        select={handleDateSelect}
         eventClick={handleEventRemove}
-        height={'90vh'}
+        height={"90vh"}
+        events={events}
       />
-
-      {modal && (
-        <div className="modal">
-          <div className="overlay"></div>
-          <form className="modal-content" onSubmit={handleSubmit}>
-            <h2>Enter Event Details</h2>
-            <input
-              type="text"
-              required
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Enter event title"
-            />
-            <button type="submit" className="close-modal">Submit</button>
-            <button className="close-modal" onClick={handleClick}>Close</button>
-          </form>
-        </div>
-      )}
     </div>
-  );
+  </div>
+
+  {modal && (
+    <div className="modal">
+      <div className="overlay"></div>
+      <form className="modal-content" onSubmit={handleSubmit}>
+        <h2>Enter Event Details</h2>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter event title"
+        />
+        <div className="modal-button">
+          <button type="submit" className="submit-modal">
+            Submit
+          </button>
+          <button className="close-modal" onClick={handleClick}>
+            Close
+          </button>
+        </div>
+      </form>
+    </div>
+  )}
+</div>
+  )
 }
